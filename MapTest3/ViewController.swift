c712d3fbda393f41bffc62195ddf45bb254742e1
atsushi-koga <stbe51@gmail.com>
2016-08-01 16:03:14 +0900

    Initial Commit

---
+++ MapTest3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapTest3
+//
+//  Created by Mahina on 2016/08/01.
+//  Copyright © 2016年 atsushi koga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
